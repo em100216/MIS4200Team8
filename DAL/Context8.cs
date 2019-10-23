78078b42d6686e38db06f0774880f24a0fff495b
mm548215 <mm548215@COB-CLAB59>
2019-10-23 10:49:51 -0400

    a

---
+++ DAL/Context8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MIS4200Team8.Models;
+using System.Data.Entity;
+
+namespace MIS4200Team8.DAL
+{
+    public class Context8 : DbContext
+    {
+        public Context8() : base ("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<UserDetail> UserDetails { get; set; }
+        
+    }
+}
