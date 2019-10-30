f17e7b597b2b35d811d5c14fc62af4a2b6954c84
em100216 <em100216@ohio.edu>
2019-10-30 11:41:32 -0400

    core value models + UI changes

---
+++ Models/CoreValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team8.Models
+{
+    public class CoreValue
+    {
+
+        public int coreValueID { get; set; }
+        public string coreValueName { get; set; }
+
+
+    }
+}
